257b3afb463deaa2eca772822e41d3410bb5458a
Yooshikii <knut-a_91@hotmail.com>
2025-12-16 10:45:05 +0100

    Added Vecnopool to addressNames.

--- app/data/addressNames.ts
+++ app/data/addressNames.ts
@@ -3,6 +3,7 @@ export const ADDRESS_NAMES: Record<string, string> = {
   "vecno:qp9wy9425nv7xy6tn9hytk839gnv3ky6nd7qhrzhg333dztgm3r4ugssphplt": "Development Fund",
   "vecno:qqlgn47f2yc96vqwwwyywlqmqrkkfpwy5p0d5d2f6dr3hlvtmgu9jmphmn353": "Team Treasury",
   "vecno:qqxxn8kj6q6efunw498vael94650ttk66hahcj8cwad79m994jrywzktmkwkg": "Listing Fund",
+  "vecno:qpsen6gmwflvcjw7ns8aefnvckv4m94aleshrs5uwkgekfmax0qzg73pkvks0": "Vecnopool",  
   "vecno:qrllllllllllllllllllllllllllllllllllllllllllllllllll7skst8zc2": "Burn Address",
 };
 
