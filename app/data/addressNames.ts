3c8d361d147888dace6c06049fcb55e7f0de0d85
Yooshikii <knut-a_91@hotmail.com>
2025-12-19 22:42:36 +0100

    Updated addressNames to show change addresses, these show as "Community Wallet 2" etc.

--- app/data/addressNames.ts
+++ app/data/addressNames.ts
@@ -1,8 +1,12 @@
 export const ADDRESS_NAMES: Record<string, string> = {
   "vecno:qp8dhhdyltaxwhu7c24gyvzs7jg34fwggvqp62zsgfvnfkqq3xdukynlm9uw5": "Community Wallet",
+  "vecno:qq7qj7mh3jzsxc3cly8h8wmjnsala6a4fw465d9kknpcpcq22np4sm5zvjjmm": "Community Wallet 2",
   "vecno:qp9wy9425nv7xy6tn9hytk839gnv3ky6nd7qhrzhg333dztgm3r4ugssphplt": "Development Fund",
+  "vecno:qqhu9seh0r9c0ny55yruka2ctzmkecvv9durclelh3qssea73497kqahdw9zk": "Development Fund 2",
   "vecno:qqlgn47f2yc96vqwwwyywlqmqrkkfpwy5p0d5d2f6dr3hlvtmgu9jmphmn353": "Team Treasury",
+  "vecno:qqlf4w8kfx6wv00hz7dsgu688mdcmcrcams0fetnanku2panxhu3uzw3vhljx": "Team Treasury 2",  
   "vecno:qqxxn8kj6q6efunw498vael94650ttk66hahcj8cwad79m994jrywzktmkwkg": "Listing Fund",
+  "vecno:qz7vlt30gj7a2q9z94sgfk2fen82ywwd7x09ltccn70znwawqv8pvdz3300kn": "Listing Fund 2",
   "vecno:qpsen6gmwflvcjw7ns8aefnvckv4m94aleshrs5uwkgekfmax0qzg73pkvks0": "Vecnopool",  
   "vecno:qrllllllllllllllllllllllllllllllllllllllllllllllllll7skst8zc2": "Burn Address",
 };
