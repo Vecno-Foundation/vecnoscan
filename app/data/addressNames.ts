2205377a412cd93337c276c4a66c63f7296aff71
Yooshikii <knut-a_91@hotmail.com>
2025-12-25 17:51:26 +0100

    Updated AddressNames.

--- app/data/addressNames.ts
+++ app/data/addressNames.ts
@@ -7,7 +7,7 @@ export const ADDRESS_NAMES: Record<string, string> = {
   "vecno:qqlf4w8kfx6wv00hz7dsgu688mdcmcrcams0fetnanku2panxhu3uzw3vhljx": "Team Treasury 2",  
   "vecno:qqxxn8kj6q6efunw498vael94650ttk66hahcj8cwad79m994jrywzktmkwkg": "Listing Fund",
   "vecno:qz7vlt30gj7a2q9z94sgfk2fen82ywwd7x09ltccn70znwawqv8pvdz3300kn": "Listing Fund 2",
-  "vecno:qpsen6gmwflvcjw7ns8aefnvckv4m94aleshrs5uwkgekfmax0qzg73pkvks0": "Vecnopool",  
+  "vecno:qqvcuc2vn3n6kgtq649gt555c35wa556wt734wurcldqt58n0yrxuq7emej2z": "Vecnopool.de",  
   "vecno:qrllllllllllllllllllllllllllllllllllllllllllllllllll7skst8zc2": "Burn Address",
 };
 
