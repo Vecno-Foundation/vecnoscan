662a2de12e498fffae6942c85c242cd73a3fb9b7
Yooshikii <knut-a_91@hotmail.com>
2026-02-27 16:42:34 +0100

    Updated address names.

--- app/data/addressNames.ts
+++ app/data/addressNames.ts
@@ -1,4 +1,5 @@
 export const ADDRESS_NAMES: Record<string, string> = {
+  // Official Wallets
   "vecno:qp8dhhdyltaxwhu7c24gyvzs7jg34fwggvqp62zsgfvnfkqq3xdukynlm9uw5": "Community Wallet",
   "vecno:qq7qj7mh3jzsxc3cly8h8wmjnsala6a4fw465d9kknpcpcq22np4sm5zvjjmm": "Community Wallet 2",
   "vecno:qp9wy9425nv7xy6tn9hytk839gnv3ky6nd7qhrzhg333dztgm3r4ugssphplt": "Development Fund",
@@ -7,10 +8,21 @@ export const ADDRESS_NAMES: Record<string, string> = {
   "vecno:qqlf4w8kfx6wv00hz7dsgu688mdcmcrcams0fetnanku2panxhu3uzw3vhljx": "Team Treasury 2",  
   "vecno:qqxxn8kj6q6efunw498vael94650ttk66hahcj8cwad79m994jrywzktmkwkg": "Listing Fund",
   "vecno:qz7vlt30gj7a2q9z94sgfk2fen82ywwd7x09ltccn70znwawqv8pvdz3300kn": "Listing Fund 2",
+
+  // Pools
   "vecno:qqvcuc2vn3n6kgtq649gt555c35wa556wt734wurcldqt58n0yrxuq7emej2z": "Vecnopool.de",
+  "vecno:qp8xz5xz9sz5c95s8gmz4vm26e98f2vkrmx6zgywgsfzwuphuvxqc3ehy28da": "NushyPool.com",
+  "vecno:qpftu54aujjjlgxqxt58h2p46x5hjjpufm8jc8yr8qwwdlfjztqxvczc4vra7": "Ninjaraider Pool",
+
+  // Dead pools.
   "vecno:qpgp07pwe540k7me5dhhtzt3mxqkuwugh3et6whs23l6dhflxk77uqp2rlpts": "Vecnopool.com",
+  
+  // Burn address
   "vecno:qrllllllllllllllllllllllllllllllllllllllllllllllllll7skst8zc2": "Burn Address",
+
+  // Exchanges
   "vecno:qr2nrsrxgueuy5wwwkhtvqm8pt95qzp268tc2a4mqyhyzuvnqlmhzc3ktm3xu": "Safetrade Exchange",
+  "vecno:qzgrt6juqxphe9k3s77cltjgs287y8jus9xg4ccepk879yfh00dd5yl48r2lu": "Dex-Trade Exchange",
 };
 
 export type AddressNames = typeof ADDRESS_NAMES;
